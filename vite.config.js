5e9fd9bec1b01d545760a5406cdf5da52cc8100a
Vaishnavi Thakur <vaaishnavi.4713@gmail.com>
2026-04-01 22:49:07 +0530

    vercel and github support

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Personal-Portfolio/', // <-- Add this line
+  base: process.env.VERCEL ? '/' : '/Personal-Portfolio/',
   plugins: [react()],
 })
